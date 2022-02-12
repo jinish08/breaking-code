e7001194ec094790732465592070fea0ca52695c
Deadpool <abcd162003@gmail.com>
2022-02-12 20:09:58 +0530

    Card component

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class Card extends Component {
+  render() {
+    return (
+      <div class="card">
+        <div class="content">
+          <h1>Tractor</h1>
+          <p>Redeem</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
--- src/pages/Redeem.js
+++ src/pages/Redeem.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
-import "../css/main.css";
+import "../css/redeem.css";
+import { Card } from "../components/Card";
 
 const Redeem = () => {
   return (
     <>
       <section class="header">
         <Navbar />
+        <div class="container1">
+          <Card />
+          <Card />
+        </div>
       </section>
     </>
   );
