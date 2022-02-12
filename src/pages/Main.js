94ba2603af37372276486885639ff8a0dc315280
Deadpool <abcd162003@gmail.com>
2022-02-12 14:28:42 +0530

    added main

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Main</h1>
+    </>
+  );
+};
+
+export default Main;
