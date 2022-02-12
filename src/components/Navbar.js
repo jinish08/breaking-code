40b473217ef3e36744b57385f356a13548c5048c
Deadpool <abcd162003@gmail.com>
2022-02-13 00:20:56 +0530

    design changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,12 +21,12 @@ export const Navbar = () => {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+              <a class="nav-link active" aria-current="page" href="main">
                 Home
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link active" href="/">
+              <a class="nav-link active" href="redeem">
                 Redeem
               </a>
             </li>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,18 +9,18 @@ const Main = () => {
         <Navbar />
         <div class="container">
           <div class="card">
-            <div class="card-body">
+            <div class="card-body" id="Sell">
               <h5 class="card-title">Sell Waste</h5>
-              <p class="card-text">Sell your waste to get Points!!!</p>
+              <p class="card-text">Sell your waste to get Points</p>
               <a href="/sell" class="btn btn-primary">
                 Sell
               </a>
             </div>
           </div>
           <div class="card">
-            <div class="card-body">
+            <div class="card-body" id="Redeem">
               <h5 class="card-title">Redeem Points</h5>
-              <p class="card-text">Click here to redeem points</p>
+              <p class="card-text">Click here to redeem Points</p>
               <a href="/redeem" class="btn btn-primary">
                 Redeem
               </a>
